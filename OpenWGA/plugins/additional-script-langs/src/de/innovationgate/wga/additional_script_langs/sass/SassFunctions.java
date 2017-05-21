94e95bdbd7671b5f379e66c561dbdc692ec7e063
ws <ws@innovationgate.com>
2017-05-21 16:22:49 +0200

    added try-catch

--- OpenWGA/plugins/additional-script-langs/src/de/innovationgate/wga/additional_script_langs/sass/SassFunctions.java
+++ OpenWGA/plugins/additional-script-langs/src/de/innovationgate/wga/additional_script_langs/sass/SassFunctions.java
@@ -6,6 +6,7 @@ import org.jruby.RubyHash;
 import de.innovationgate.webgate.api.WGException;
 import de.innovationgate.webgate.api.WGScriptModule;
 import de.innovationgate.wga.server.api.Design;
+import de.innovationgate.wga.server.api.WGA;
 
 public class SassFunctions {
     
@@ -13,14 +14,27 @@ public class SassFunctions {
         
         Ruby runtime = org.jruby.Ruby.getGlobalRuntime();
         Design design = (Design) options.get(org.jruby.RubySymbol.newSymbol(runtime, "wgaDesign"));
-        return design.resolve(name).scriptURL(WGScriptModule.CODETYPE_CSS);
+        try{
+        	return design.resolve(name).scriptURL(WGScriptModule.CODETYPE_CSS);
+        }
+        catch(Exception e){
+        	WGA wga = (WGA) options.get(org.jruby.RubySymbol.newSymbol(runtime, "wga"));
+        	wga.getLog().error("wga_css_url: css not found: " + name);
+        	return "/* css not found: " + name + " */";        	
+        }
     }
     
     public static String wga_file_url(String db, String container, String name, RubyHash options) throws WGException {
-        
         Ruby runtime = org.jruby.Ruby.getGlobalRuntime();
         Design design = (Design) options.get(org.jruby.RubySymbol.newSymbol(runtime, "wgaDesign"));
-        return design.resolve(db, container).fileURL(name);
+        try{
+	        return design.resolve(db, container).fileURL(name);
+        }
+        catch(Exception e){
+        	WGA wga = (WGA) options.get(org.jruby.RubySymbol.newSymbol(runtime, "wga"));
+        	wga.getLog().error("wga_file_url: file not found: " + db + "/" + container + "/" + name);
+        	return "/* file not found: " + db + "/" + container + "/" + name + " */";        	
+        }
     }
 
 }
