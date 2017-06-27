6656605b2856ac004ecefeab01a6bfaf6276cf1d
ws <ws@innovationgate.com>
2017-06-27 16:20:01 +0200

    #00005033: Browser preferred language behaviour uses "host based preferred languages"

--- OpenWGA/libraries/wga/src/main/java/de/innovationgate/wgpublisher/lang/BrowserLocaleLanguageBehaviour.java
+++ OpenWGA/libraries/wga/src/main/java/de/innovationgate/wgpublisher/lang/BrowserLocaleLanguageBehaviour.java
@@ -2,6 +2,8 @@ package de.innovationgate.wgpublisher.lang;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
@@ -10,16 +12,24 @@ import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 
+import de.innovationgate.utils.WGUtils;
 import de.innovationgate.webgate.api.WGAPIException;
 import de.innovationgate.webgate.api.WGBackendException;
 import de.innovationgate.webgate.api.WGContent;
 import de.innovationgate.webgate.api.WGDatabase;
+import de.innovationgate.webgate.api.WGException;
 import de.innovationgate.webgate.api.WGLanguage;
 import de.innovationgate.webgate.api.WGStructEntry;
+import de.innovationgate.wga.config.VirtualHost;
+import de.innovationgate.wga.config.WGAConfiguration;
+import de.innovationgate.wga.server.api.WGA;
+import de.innovationgate.wgpublisher.filter.WGAVirtualHostingFilter;
 import de.innovationgate.wgpublisher.labels.WGAResourceBundleManager;
 import de.innovationgate.wgpublisher.webtml.utils.TMLContext;
 
-public class BrowserLocaleLanguageBehaviour implements LanguageBehaviour {
+public class BrowserLocaleLanguageBehaviour implements LanguageBehaviour, InitializableLanguageBehaviour {
+
+	private WGA _wga;
 
 	@Override
 	public WGContent requestSelectContentForPage(WGStructEntry page, HttpServletRequest req, boolean isBI) throws WGAPIException {
@@ -28,21 +38,33 @@ public class BrowserLocaleLanguageBehaviour implements LanguageBehaviour {
         if (content != null) {
             return content;
         }
-        else {
-            return LanguageBehaviourTools.getRelevantContent(page, page.getDatabase().getDefaultLanguage(), isBI);
-        }
+
+        // v-host fallback language
+		for(String lang: getVhostPreferedLanguages(req)){
+			content = LanguageBehaviourTools.getRelevantContent(page, lang, isBI);
+	        if (content != null) 
+	            return content;
+		}
+		
+       	return LanguageBehaviourTools.getRelevantContent(page, page.getDatabase().getDefaultLanguage(), isBI);
+
 	}
 
 	@Override
 	public WGContent requestSelectContentForName(WGDatabase db, HttpServletRequest req, String name, boolean isBI) throws WGAPIException {
         
 		WGContent content = LanguageBehaviourTools.chooseNamedContentByRequestLocales(db, name, req, isBI);
-        if (content != null) {
+        if (content != null) 
             return content;
-        }
-        else {
-            return db.getContentByName(name, db.getDefaultLanguage());
-        }
+        
+        // v-host language
+		for(String lang: getVhostPreferedLanguages(req)){
+			content = db.getContentByName(name, lang);
+	        if (content != null) 
+	            return content;
+		}
+		
+        return db.getContentByName(name, db.getDefaultLanguage());
 	}
 
 	@Override
@@ -52,61 +74,39 @@ public class BrowserLocaleLanguageBehaviour implements LanguageBehaviour {
         if (lang != null && !lang.isDummy()) {
             return lang;
         }
-        lang = db.getLanguage(db.getDefaultLanguage());
-        if (lang != null && !lang.isDummy()) {
-            return lang;
+        
+        // v-host language
+        for(String langname: getVhostPreferedLanguages(req)){
+			lang = db.getLanguage(langname);
+	        if (lang != null && !lang.isDummy()) 
+	            return lang;        	
         }
+
+		lang = db.getLanguage(db.getDefaultLanguage());
+        if (lang != null && !lang.isDummy()) 
+            return lang;
+
         return null;
 	}
 
 	@Override
 	public WGContent webtmlSelectContentForPage(WGStructEntry page, TMLContext context, boolean isBI) throws WGAPIException {
 
-		WGContent content = null;
-		content = LanguageBehaviourTools.chooseContentByRequestLocales(page, context.getrequest(), isBI);
-        if (content != null) {
-            return content;
-        }
-        // Database default language
-        String defaultLangName = page.getDatabase().getDefaultLanguage();
-        content = LanguageBehaviourTools.getRelevantContent(page, defaultLangName, isBI);
-        if (content != null) {
-            return content;
-        }
-        return null;
+		return requestSelectContentForPage(page, context.getrequest(), isBI);
+		
 	}
 
 	@Override
 	public WGContent webtmlSelectContentForName(WGDatabase db, TMLContext context, String name, boolean isBI) throws WGAPIException {
 
-		WGContent content = null;
-        content = LanguageBehaviourTools.chooseNamedContentByRequestLocales(db, name, (HttpServletRequest) context.getrequest(), isBI);
-        if (content != null) {
-            return content;
-        }
-
-        // Database default language
-        content = db.getContentByName(name, db.getDefaultLanguage());
-        if (content != null) {
-            return content;
-        }
-        return null;
+		return requestSelectContentForName(db, context.getrequest(), name, isBI);
+		
 	}
 
 	@Override
 	public WGLanguage webtmlSelectDatabaseLanguage(WGDatabase db, TMLContext context) throws WGAPIException {
 
-		WGLanguage lang = null;
-		lang = LanguageBehaviourTools.chooseLanguageByRequestLocales(db, context.getrequest());
-        if (lang != null && !lang.isDummy()) {
-            return lang;
-        }
-        // Database default language
-        lang = db.getLanguage(db.getDefaultLanguage());
-        if (lang != null && !lang.isDummy()) {
-            return lang;
-        }
-        return null;
+		return requestSelectDatabaseLanguage(db, context.getrequest());
 	}
 
 	@Override
@@ -117,36 +117,45 @@ public class BrowserLocaleLanguageBehaviour implements LanguageBehaviour {
         while (locales.hasMoreElements()) {
             Locale locale = locales.nextElement();
             try {
-                
                 label = LanguageBehaviourTools.fetchLabelForLanguage(manager, container, file, key, locale);
                 if (label != null) {
                     return label;
                 }
             }
             catch (IOException e) {
-                context.getlog().error("Exception retrieving label " + container + "/" + file + "/" + key + " for language " + locale.toString() + " from DB " + manager.getDb().getDbReference(), e);
+                context.getlog().error("Exception retrieving label " + container + "/" + file + "/" + key + " for language " + locale.toString() + " from app " + manager.getDb().getDbReference(), e);
             }
         }
         
+        // v-host fallback language
+        for(String langname: getVhostPreferedLanguages(context.getrequest())){
+			Locale locale = WGLanguage.languageNameToLocale(langname);
+	        try {
+	            label = LanguageBehaviourTools.fetchLabelForLanguage(manager, container, file, key, locale);
+	            if (label != null) 
+	                return label;
+	        }
+	        catch (IOException e) {
+	            context.getlog().error("Exception retrieving label " + container + "/" + file + "/" + key + " for language " + locale.toString() + " from app " + manager.getDb().getDbReference(), e);
+	        }
+		}
+
         // Database default language
         Locale defLangLocale = WGLanguage.languageNameToLocale(manager.getDb().getDefaultLanguage());
         try {
-            
             label = LanguageBehaviourTools.fetchLabelForLanguage(manager, container, file, key, defLangLocale);
-            if (label != null) {
+            if (label != null) 
                 return label;
-            }
         }
         catch (IOException e) {
-            context.getlog().error("Exception retrieving label " + container + "/" + file + "/" + key + " for language " + defLangLocale.toString() + " from DB " + manager.getDb().getDbReference(), e);
+            context.getlog().error("Exception retrieving label " + container + "/" + file + "/" + key + " for language " + defLangLocale.toString() + " from app " + manager.getDb().getDbReference(), e);
         }
 
         // Label fallback language
         try {
             label = LanguageBehaviourTools.fetchLabelForFallbackLanguage(manager, container, file, key, true);
-            if (label != null) {
+            if (label != null) 
                 return label;
-            }
         }
         catch (IOException e) {
             throw new WGBackendException("Exception retrieving label", e);
@@ -167,6 +176,13 @@ public class BrowserLocaleLanguageBehaviour implements LanguageBehaviour {
                 langs.add(lang);
             }
         }
+
+        // v-host fallback language
+        for(String langname: getVhostPreferedLanguages(context.getrequest())){
+        	WGLanguage lang = db.getLanguage(langname);
+        	if(lang!=null && !lang.isDummy())
+        		langs.add(lang);
+        }
         
         langs.add(db.getLanguage(db.getDefaultLanguage()));
         
@@ -178,13 +194,27 @@ public class BrowserLocaleLanguageBehaviour implements LanguageBehaviour {
 	public String webtmlGetPreferredLanguage(WGDatabase db, TMLContext context) throws WGAPIException {
 
         WGLanguage lang = requestSelectDatabaseLanguage(db, context.getrequest());
-        if (lang != null) {
+        if (lang != null) 
             return lang.getName();
-        }
-        else {
-            return db.getDefaultLanguage();
-        }
+        
+        return null;
 
+	}
+
+	@Override
+	public void init(WGA wga, WGDatabase db) throws WGException {
+		_wga = wga;		
+	}
+
+	private List<String> getVhostPreferedLanguages(HttpServletRequest request){
+		WGAConfiguration config = _wga.getCore().getWgaConfiguration();
+		VirtualHost vhost = WGAVirtualHostingFilter.findMatchingHost(config, request);
+		if(vhost!=null){
+			String langs = vhost.getPreferedLanguages();
+			if(langs!=null)
+				return Arrays.asList(langs.split("\\s*,\\s*")); 
+		}
+		return Collections.emptyList();
 	}
 
 }
