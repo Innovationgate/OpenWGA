2cd9230bbb9ed74d1f0313dc8331685c689f32d1
ws <ws@innovationgate.com>
2018-09-18 19:59:10 +0200

    #00005257: CM-Neo: deactivate menu "userdefined-settings" if no settings available

--- OpenWGA/plugins/cm-neo/design/scripts/js/toolbar.js
+++ OpenWGA/plugins/cm-neo/design/scripts/js/toolbar.js
@@ -67,7 +67,9 @@ define(["cm", "sitepanel", "jquery", "outline", "bootstrap"], function(CM, Sitep
 		$("#toolbars [data-action-group='create']")[ev.params.structkey ? "removeClass" : "addClass"]("disabled")
 		$("#toolbars [data-action-group='delete']")[ev.params.structkey ? "removeClass" : "addClass"]("disabled")
 		$("#toolbars [data-action-group='clipboard']")[ev.params.structkey ? "removeClass" : "addClass"]("disabled")
-			
+		
+		$("#toolbars [data-action='settings-user-defined']").parent()[ev.params.has_userdefined_settings ? "removeClass" : "addClass"]("disabled")
+		
 	})
 
 	/* Click handler */
