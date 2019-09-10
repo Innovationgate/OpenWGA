a8b74a61e3019fbade15c89ab40e980a82a94ade
ws <ws@innovationgate.com>
2019-09-10 21:34:19 +0200

    #00005436

--- OpenWGA/libraries/wga/src/main/java/de/innovationgate/wgpublisher/webtml/Range.java
+++ OpenWGA/libraries/wga/src/main/java/de/innovationgate/wgpublisher/webtml/Range.java
@@ -469,12 +469,9 @@ public class Range extends Base {
                 
                 boolean useCache = false;
                 if (CACHEUPDATE_DB.equals(cacheUpdate) || CACHEUPDATE_CONTENT.equals(cacheUpdate)) {
-                	if(testedDate==null){
-                		log.error("testedDate==null. cacheUpdate is " + cacheUpdate + " - Error retrieving WebTML cache data for key "  + getTMLContext().db().getDbReference() + "/" + status._currentCacheId + "/" + status._currentCacheKey);
-                	}
-                	else{
-                		useCache = cacheEntry.getDate().getTime() >= testedDate.getTime();
-                	}
+                	if(testedDate==null)
+                		useCache = true;
+                	else useCache = cacheEntry.getDate().getTime() >= testedDate.getTime();
                 }
                 else {
                     useCache = true;
