393374b17392889256af60d09c468487e967c70e
ws <ws@innovationgate.com>
2020-10-11 11:16:23 +0200

    #00005614: Added "extends AuthenticationModule" to class AnonymousAwareAuthenticationModule

--- OpenWGA/libraries/wgapi/src/main/java/de/innovationgate/webgate/api/auth/AnonymousAwareAuthenticationModule.java
+++ OpenWGA/libraries/wgapi/src/main/java/de/innovationgate/webgate/api/auth/AnonymousAwareAuthenticationModule.java
@@ -2,7 +2,7 @@ package de.innovationgate.webgate.api.auth;
 
 import javax.servlet.http.HttpServletRequest;
 
-public interface AnonymousAwareAuthenticationModule{
+public interface AnonymousAwareAuthenticationModule extends AuthenticationModule{
 
 	public AuthenticationSession anonymousLogin(HttpServletRequest request) throws AuthenticationException;
 
