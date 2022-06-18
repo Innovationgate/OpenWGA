1a3170f30919531d3074f4fac5324bfa8a9e7222
ws <ws@innovationgate.com>
2022-06-18 15:31:58 +0200

    #00005928: Re-Implementation

--- OpenWGA/libraries/tmlScript/src/main/java/de/innovationgate/wgpublisher/expressions/tmlscript/wrapping/ListWrapper.java
+++ OpenWGA/libraries/tmlScript/src/main/java/de/innovationgate/wgpublisher/expressions/tmlscript/wrapping/ListWrapper.java
@@ -1,61 +1,66 @@
 package de.innovationgate.wgpublisher.expressions.tmlscript.wrapping;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
 import de.innovationgate.ext.org.mozilla.javascript.Context;
 import de.innovationgate.ext.org.mozilla.javascript.Function;
-import de.innovationgate.ext.org.mozilla.javascript.JavaScriptException;
+import de.innovationgate.ext.org.mozilla.javascript.FunctionObject;
 import de.innovationgate.ext.org.mozilla.javascript.NativeJavaObject;
 import de.innovationgate.ext.org.mozilla.javascript.Scriptable;
 import de.innovationgate.ext.org.mozilla.javascript.ScriptableObject;
-import de.innovationgate.ext.org.mozilla.javascript.Wrapper;
-import de.innovationgate.webgate.api.WGException;
 import de.innovationgate.wgpublisher.expressions.tmlscript.JavascriptFunctionComparator;
 
-public class ListWrapper extends ScriptableObject implements Wrapper {
+public class ListWrapper extends NativeJavaObject {
 
 	private static final long serialVersionUID = 1L;
 	private ArrayList<Object> list;
 
-    public static final String[] METHODS = {
-            "sort",
-            "reverse"
-        };
+	private Function _sortFunction;
+	private Function _reverseFunction;
 
-	public ListWrapper(Scriptable scope, ArrayList<Object> list) {
-		super(scope, new NativeJavaObject(scope, list, ArrayList.class));
-		defineFunctionProperties(METHODS, ListWrapper.class, DONTENUM);
+	public ListWrapper(Scriptable scope, ArrayList<Object> list){
+		super(scope, list, ArrayList.class);
+
 		this.list = list;
+		
+		try {
+			Method method = this.getClass().getMethod("sort", Function.class);
+			_sortFunction = new FunctionObject("sort", method, this);
+			method = this.getClass().getMethod("reverse");
+			_reverseFunction = new FunctionObject("reverse", method, this);
+		} catch (NoSuchMethodException | SecurityException e) {
+			e.printStackTrace();
+		} 
 	}
 
-	@SuppressWarnings("unchecked")
-	public static ArrayList<Object> sort(Context cx, Scriptable thisObj, java.lang.Object[] args, Function funObj) throws JavaScriptException, WGException, NoSuchMethodException, SecurityException {
-		ArrayList<Object> list = ((ListWrapper)thisObj).unwrap();		
+	public ArrayList<Object> sort(Function f){
 		Comparator<Object> compare;
-		if(args.length > 0 && args[0] instanceof Function){
-			compare = new JavascriptFunctionComparator((Function) args[0]);
+		if(f==null){
+			compare = new Comparator<Object>() {
+				@Override
+				public int compare(Object o1, Object o2) {
+					return o1.toString().compareTo(o2.toString());
+				}
+			};
 		}
-		else compare = new Comparator<Object>() {
-			@Override
-			public int compare(Object o1, Object o2) {
-				String s1 = o1.toString();
-				String s2 = o2.toString();
-				return s1.compareTo(s2);
-			}
-		};
+		else compare = new JavascriptFunctionComparator(f);
 		list.sort(compare);
 		return list;
-		
 	}
 
-	public static ArrayList<Object> reverse(Context cx, Scriptable thisObj, java.lang.Object[] args, Function funObj) throws JavaScriptException, WGException, NoSuchMethodException, SecurityException {
-		ArrayList<Object> list = ((ListWrapper)thisObj).unwrap();
+	public ArrayList<Object> reverse(){
 		Collections.reverse(list);
 		return list;
 	}
 
+	public ArrayList<Object> unwrap() {
+		return this.list;
+	}
+		
+	
 	/**
 	 * @see ScriptableObject#getClassName()
 	 */
@@ -63,9 +68,16 @@ public class ListWrapper extends ScriptableObject implements Wrapper {
 		return "de.innovationgate.wgpublisher.expressions.ListWrapper";
 	}
 
-	/**
-	 * @see ScriptableObject#get(String, Scriptable)
-	 */
+	public Object get(String prop, Scriptable arg1) {
+		if(prop.equals("sort")){
+			return _sortFunction;
+		}
+		else if(prop.equals("reverse")){
+			return _reverseFunction;
+		}
+		else return RhinoWrapFactory.notFoundToNull(super.get(prop, arg1));
+	}
+	
 	public Object get(int index, Scriptable arg1) {
 		if (super.has(index, arg1)) {
 			return RhinoWrapFactory.notFoundToNull(super.get(index, arg1));
@@ -77,12 +89,6 @@ public class ListWrapper extends ScriptableObject implements Wrapper {
 
 	public void put(int index, Scriptable start, Object value) {
         this.list.set(index, Context.jsToJava(value, Object.class));
-	}
-
-	@Override
-	public ArrayList<Object> unwrap() {
-		// TODO Auto-generated method stub
-		return this.list;
 	}
 
 }
