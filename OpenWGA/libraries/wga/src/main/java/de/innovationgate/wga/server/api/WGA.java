7cde9f3b4f162906f42e248ca8eaddb45f7d2e58
ws <ws@innovationgate.com>
2022-06-20 23:28:38 +0200

    #00005930, #00005929

--- OpenWGA/libraries/wga/src/main/java/de/innovationgate/wga/server/api/WGA.java
+++ OpenWGA/libraries/wga/src/main/java/de/innovationgate/wga/server/api/WGA.java
@@ -3409,8 +3409,30 @@ public class WGA {
     protected WGAContext getContext() {
         return _context;
     }
+    
+    // WGA.Date
+    public WGADate Date(){
+    	return new WGADate(this);
+    }
+    public WGADate Date(Date d){
+    	return new WGADate(this, d);
+    }
+    public WGADate Date(String date, String format, String language) throws ParseException, WGException{
+    	return new WGADate(this, date, format, language);
+    }
+    public WGADate Date(String date, String format) throws ParseException, WGException{
+    	return new WGADate(this, date, format);
+    }
 
-
+    // WGA.List
+    public WGAList List(Collection<Object> list){
+    	return new WGAList(list);
+    }
+    public WGAList List(){
+    	return new WGAList();
+    }
+    
+    // WGA.File
     public WGAFile File(String filename){
     	return new WGAFile(this, filename);
     }
@@ -3418,14 +3440,17 @@ public class WGA {
     	return new WGAFile(this, file);
     }
  
+    // WGA.HttpClient
     public WGAHttpClient HttpClient(String url) throws WGException{
     	return new WGAHttpClient(this, url);
     }
  
+    // WGA.TempFile
     public WGATempFile TempFile(String filename) throws IOException{
     	return new WGATempFile(this, filename);
     }
      
+    // WGA.Logger
     public WGALogger Logger(String name){
     	return new WGALogger("wga."+name);
     }
