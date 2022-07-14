fd2c84e64ca032414edeaf147ac340f7db7c77e5
ws <ws@innovationgate.com>
2022-07-14 17:50:02 +0200

    Added diff() and age()

--- OpenWGA/libraries/wga/src/main/java/de/innovationgate/wga/server/api/WGADate.java
+++ OpenWGA/libraries/wga/src/main/java/de/innovationgate/wga/server/api/WGADate.java
@@ -43,4 +43,23 @@ public class WGADate extends Date{
 		return this;
 	}
 
+	public Long age(String unit){
+		return diff(new Date(), unit);
+	}
+	
+	public Long diff(Date otherDate, String unit){
+		Long diff = otherDate.getTime()-getTime();	// milli seconds
+		if(unit.equals("w"))
+			return  diff / 1000 / 60 / 60 / 24 / 7;
+		if(unit.equals("d"))
+			return  diff / 1000 / 60 / 60 / 24;
+		if(unit.equals("h"))
+			return  diff / 1000 / 60 / 60;
+		if(unit.equals("m"))
+			return  diff / 1000 / 60;
+		if(unit.equals("s"))
+			return  diff / 1000;
+		return diff;
+	}
+	
 }
