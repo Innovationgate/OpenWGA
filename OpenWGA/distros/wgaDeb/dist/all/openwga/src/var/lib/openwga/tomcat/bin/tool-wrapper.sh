f243d57228b7c7567e4d04b2bb7ad083727dcc5c
ws <ws@innovationgate.com>
2022-08-07 00:30:58 +0200

    #00005976: Updated tomcat to version 9

--- OpenWGA/distros/wgaDeb/dist/all/openwga/src/var/lib/openwga/tomcat/bin/tool-wrapper.sh
+++ OpenWGA/distros/wgaDeb/dist/all/openwga/src/var/lib/openwga/tomcat/bin/tool-wrapper.sh
@@ -119,7 +119,7 @@ fi
 if [ ! -z "$CLASSPATH" ] ; then
   CLASSPATH="$CLASSPATH":
 fi
-CLASSPATH="$CLASSPATH""$CATALINA_HOME"/bin/bootstrap.jar:"$CATALINA_HOME"/bin/tomcat-juli.jar:"$CATALINA_HOME"/lib/servlet-api.jar:"$CATALINA_HOME"/lib/tomcat-coyote.jar
+CLASSPATH="$CLASSPATH""$CATALINA_HOME"/bin/bootstrap.jar:"$CATALINA_HOME"/bin/tomcat-juli.jar:"$CATALINA_HOME"/lib/servlet-api.jar:"$CATALINA_HOME"/lib/tomcat-util.jar
 
 # For Cygwin, switch paths to Windows format before running java
 if $cygwin; then
